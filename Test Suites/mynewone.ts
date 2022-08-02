31055c9f25bb49116f15bd3e288cdc87201fe3c0
avengers12345 <110410211+avengers12345@users.noreply.github.com>
2022-08-02 23:44:30 +0800

    sas

--- Test Suites/mynewone.ts
+++ Test Suites/mynewone.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>e2f4b500-9f7a-4464-bd77-30e545836fc2</testSuiteGuid>
+   <testCaseLink>
+      <guid>202195da-24a7-4786-bd1a-e584d2a2ea12</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/mine</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
