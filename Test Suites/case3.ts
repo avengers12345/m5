00152bcf2d792afbd89e5b6f797f3233e8ddf51f
avengers12345 <110410211+avengers12345@users.noreply.github.com>
2022-08-02 16:08:43 +0800

    hjj

---
+++ Test Suites/case3.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>case3</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>53d09981-1e58-4d2a-a5a6-1cfc5b178c57</testSuiteGuid>
+</TestSuiteEntity>
